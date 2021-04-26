d6bc2e2537e3d0c812e14036c52dc0d7f9eed775
Kritsana135 <61050135@kmitl.ac.th>
2021-04-26 21:32:16 +0700

    add logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
-import '../styles/globals.css'
+import Head from "next/head"
+import "../styles/globals.css"
 import "../styles/home.css"
 
-
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/cs-logo.webp" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
